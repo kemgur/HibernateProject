d24e70ff13bc31e0232491858dae22f74f3317ea
kemalgurler <kemgur52@gmail.com>
2021-05-25 22:17:00 -0400

    v1_May25

---
+++ src/main/java/com/techproed/hibernatetutoringwinter/App.java
@@ -0,0 +1,13 @@
+package com.techproed.hibernatetutoringwinter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
